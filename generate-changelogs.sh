2f97f690f01889ee7fe46e463fc39408e87fa631
Crustack <crustackk@gmail.com>
2026-02-22 13:41:56 +0100

    Include pull requests without issues in changelogs

--- generate-changelogs.sh
+++ generate-changelogs.sh
@@ -5,9 +5,9 @@ OUTPUT_FILE=$2
 GITHUB_TOKEN=${3:-$CHANGELOG_GITHUB_TOKEN}
 echo "Generating CHANGELOG.md entry since $VERSION_NAME to $OUTPUT_FILE"
 timeout 90s github_changelog_generator -u Crustack -p NotallyX --since-tag "$VERSION_NAME" \
-  --no-pull-requests --include-tags-regex '^v\d+(\.\d+)*$' \
+  --no-pr-wo-labels --no-issues-wo-labels --include-tags-regex '^v\d+(\.\d+)*$' \
   --include-labels enhancement,bug --exclude-labels duplicate,question,invalid,wontfix,'already done' \
-  --enhancement-label "### Added Features" --bugs-label "### Fixed Bugs" \
+  --enhancement-label "### Added Features" --bugs-label "### Fixed Bugs" --pr-label "### Added Features" \
   --base "$OUTPUT_FILE" --output "$OUTPUT_FILE" --token "$GITHUB_TOKEN"
 
 # Check if timeout caused failure
